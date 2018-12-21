bfc1ebddee6d0f43aa882a8dfe6106c9dcd415ef
Frank Emrich <frank.emrich@ed.ac.uk>
2018-12-21 18:18:03 +0100

    Refactored frontend infrastructure to work on multiple modules

--- core/ir.mli
+++ core/ir.mli
@@ -101,6 +101,8 @@ and handler_depth = | Deep of (binder * value) list | Shallow
 val binding_scope : binding -> scope
 val binder_of_fun_def : fun_def -> binder
 
+val binders_of_binding : binding -> binder list
+
 val tapp : value * tyarg list -> value
 
 val letm : binder * tail_computation -> binding
@@ -125,3 +127,5 @@ val string_of_program : program -> string
 
 type eval_fun_def = var_info * (var list * computation) * Var.var option * location
   [@@deriving show]
+
+val program_of_bindings : binding list -> program
